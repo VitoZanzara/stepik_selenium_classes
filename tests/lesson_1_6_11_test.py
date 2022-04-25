02bf5d91cff7e412f296a6f9cd1da41d7a1cf925
zanzara <zanzara@zanzara>
2022-04-25 12:09:04 +0300

    add tests

--- tests/lesson_1_6_11_test.py
+++ tests/lesson_1_6_11_test.py
@@ -6,11 +6,14 @@ right_answer = "Congratulations! You have successfully registered!"
 url_good = 'http://suninjuly.github.io/registration1.html'
 url_bad = 'http://suninjuly.github.io/registration2.html'
 
+
 class Lesson_1_6_11_test(TestCase):
     def test_passed(self):
+        """успешная регистрации на правильной  форме"""
         self.assertEqual(check_regform_required_fields(url_good), right_answer)
 
     def test_exception(self):
+        """проверка выдачи нужного исключения на форме с багом"""
         with self.assertRaises(NoSuchElementException):
             check_regform_required_fields(url_bad)
 
